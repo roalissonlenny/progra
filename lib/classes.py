1a68c2960e5bd757e18ff70478758136fe65c4cb
Isaac López Nolasco <munoza928@gmail.com>
2024-02-01 08:35:46 -0600

    cambio 2

--- lib/classes.py
+++ lib/classes.py
@@ -17,9 +17,44 @@ class articulo():
         self.precio=price
         return 0
 
+    def setpeso(self, peso):
+        self.peso=peso
+        return 0
 
+    def setdcto(self, dcto):
+        self.dcto=dcto 
+        return 0
+
+    def setinv(self, inv):
+        self.inv=inv
+        return 0
+
+    def getpricedcto(self):
+        if self.dcto != None:
+            preciodcto= self.precio - (self.precio * (self.dcto/100))
+        else:
+            preciodcto= self.precio
+        return preciodcto
 
 
 class carritou():
+    def __init__(self, idCart):
+        self.idCart = idCart
+        self.articulos = []
+        pass
+
+    def __str__(self):
+        printcarrito = f'Csrrito numero: {self.idCart}'
+        for i in range(0, len(self.articulos), 1):
+            printcarrito += f"articulos: {self.articulos[i]}"
+        
+        return printcarrito
+
+    def addarticulo(self, idArt):
+        self.articulos.append( idArt )
+        return 0
+
+
+
     pass
     
--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from lib import *
 
 art1=articulo(10256,"Coca-cola","Canada Dry")
-print(art1)
 art1.setPrecio(25.00)
-print(art1)
+art1.setpeso(350)
+art1.setdcto(10)
+art1.setinv(10)
+#print(art1)
+
+cart1=carritou("ABC123")
+print(cart1)
+
+cart2=carritou("DEF456")
+print(cart2)
+
+cart3=carritou("GHI789")
+print(cart3)
+
+art2=articulo(12346,"Bimbo","Pan blanco")
+art2.setPrecio(30.00)
+art2.setpeso(60)
+art2.setinv(15)
+#print(art2)
+
+art3=articulo(13590,"Barcel","Rufles")
+art3.setPrecio(27.00)
+art3.setpeso(25)
+art3.setdcto(14)
+art3.setinv(35)
+#print(art3)
+
+cart1.addarticulo(art1.idArt)
+cart1.addarticulo(art2.idArt)
+cart1.addarticulo(art3.idArt)
+
+print(cart1)
+
+print(cart2)
 
